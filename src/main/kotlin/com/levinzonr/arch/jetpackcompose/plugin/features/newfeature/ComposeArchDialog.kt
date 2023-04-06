465108942ce083a0515945e431483967d7af634c
Roman Levinzon <levinzon.roman@gmail.com>
2023-04-06 16:52:49 +0200

    feat: Update the UI for the Arch dialog

--- src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/ComposeArchDialog.kt
+++ src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/ComposeArchDialog.kt
@@ -13,28 +13,31 @@ class ComposeArchDialog(
     init {
         init()
         viewModel
-                .successFlow
-                .onEach { close(0) }
-                .launchIn(dialogScope)
+            .successFlow
+            .onEach { close(0) }
+            .launchIn(dialogScope)
     }
 
     override fun createPanel(): DialogPanel {
         return panel {
             row { label("New Jetpack Compose Feature") }
             row { textField(viewModel::name).focused() }
-            row { checkBox("Also create package for the feature", viewModel::createFeaturePackage) }
 
-            noteRow("Creates a set of files for the new Feature.\n" +
-                    " All files will be placed in the package with the same name as the feature")
+            noteRow(
+                "Creates a set of files for the new Feature.\n" +
+                        " All files will be placed in the package with the same name as the feature"
+            )
+            titledRow("Options") {
+                row { checkBox("Also create package for the feature", viewModel::createFeaturePackage) }
 
-            hideableRow("Experimental") {
-                checkBox(
-                    text = "Use collectAsStateWithLifecycle",
-                    prop = viewModel::flowWithLifecycleEnabled,
-                    comment = buildString {
-                        appendLine("Collect the flow in the Route component in a lifecycle aware way")
-                        append("Requires opt-in")
-                    })
+                row {
+                    checkBox(
+                        text = "Use collectAsStateWithLifecycle",
+                        prop = viewModel::flowWithLifecycleEnabled,
+                        comment = buildString {
+                            appendLine("Collect the flow in the Route component in a lifecycle aware way")
+                        })
+                }
             }
 
         }
