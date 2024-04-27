a744e95304867d5ecb4f232a302ef97416384fbe
HanBI24 <alsltjdrhs@naver.com>
2024-04-27 21:53:58 +0900

    Add usePreviewParameterProvider boolean value in FeatureConfiguration and set Repositoy

--- src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/domain/FeatureConfiguration.kt
+++ src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/domain/FeatureConfiguration.kt
@@ -2,5 +2,6 @@ package com.levinzonr.arch.jetpackcompose.plugin.features.newfeature.domain
 
 data class FeatureConfiguration(
     val useCollectFlowWithLifecycle: Boolean,
+    val usePreviewParameterProvider: Boolean,
     val injection: InjectionConfiguration
 )
