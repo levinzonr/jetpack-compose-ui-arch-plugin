0c2bceab844a38c16a1483b7dcfb5d19607254bf
Roman Levinzon <levinzon.roman@gmail.com>
2025-11-13 15:33:05 +0100

    fix(#39): Handle cases when plugin actions activated on Files and not on Directories

--- src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newcomponent/ComposeComponentAction.kt
+++ src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newcomponent/ComposeComponentAction.kt
@@ -5,13 +5,21 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
 import com.levinzonr.arch.jetpackcompose.plugin.dependencies.ProjectDependencies
 import com.levinzonr.arch.jetpackcompose.plugin.features.newcomponent.ComposeComponentViewModel
 
 class ComposeComponentAction: AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val directory = e.getData(CommonDataKeys.PSI_ELEMENT) as PsiDirectory
+        val data = e.getData(CommonDataKeys.PSI_ELEMENT)
+
+        val directory = when(data) {
+            is PsiDirectory -> data
+            is PsiFile -> data.parent
+            else -> null
+        } ?: return
+
         val viewModel = ComposeComponentViewModel(directory, ProjectDependencies(e.project))
         ComposeComponentDialog(viewModel).show()
     }
--- src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/ui/ComposeArchAction.kt
+++ src/main/kotlin/com/levinzonr/arch/jetpackcompose/plugin/features/newfeature/ui/ComposeArchAction.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
 import com.levinzonr.arch.jetpackcompose.plugin.dependencies.ProjectDependencies
 import com.levinzonr.arch.jetpackcompose.plugin.features.newfeature.injection.AdvancedViewModelFactory
 import com.levinzonr.arch.jetpackcompose.plugin.features.newfeature.injection.ComposeArchDialogViewModelFactory
@@ -12,7 +13,14 @@ class ComposeArchAction: AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val deps = ProjectDependencies(e.project)
-        val directory = e.getData(CommonDataKeys.PSI_ELEMENT) as PsiDirectory
+        val data = e.getData(CommonDataKeys.PSI_ELEMENT)
+
+        val directory = when(data) {
+            is PsiDirectory -> data
+            is PsiFile -> data.parent
+            else -> null
+        } ?: return
+
         val viewModel = ComposeArchDialogViewModelFactory.create(directory, deps)
         val advancedViewModel = AdvancedViewModelFactory.create(deps)
         ComposeArchDialog(viewModel, advancedViewModel).show()
